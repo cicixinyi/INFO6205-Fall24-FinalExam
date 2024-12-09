2ec030636a32b8a1fa80323a39cd8bdbb5b13a3d
xxy <xu.xinyi3@northeastern.edu>
2024-12-10 04:49:52 +0800

    finish exam

--- src/main/java/org/example/Fibonacci.java
+++ src/main/java/org/example/Fibonacci.java
@@ -11,7 +11,13 @@ public class Fibonacci {
      * else <code>fibN(n-1)</code> + <code>fibN(n-2)</code>.
      */
     public long fibN(int n) {
-        return -1L;
+        if(n < 0 || n >= memo.length) return -1L;
+        if(memo[n] != -1) return memo[n];
+        else if(n < 2) return n;
+        else {
+            memo[n] = fibN(n - 1) + fibN(n - 2);
+            return memo[n];
+        }
     }
 
     /**
@@ -21,10 +27,21 @@ public class Fibonacci {
      * @return the sum of the Fibonacci numbers 0 through n.
      */
     public long sumN(int n) {
-         return -1L;
+        if(n < 0 || n > 93) return -1L;
+
+        long sum =0;
+        for(int i=0; i<=n; i++) {
+            sum += fibN(i);
+        }
+        return sum;
     }
 
     public Fibonacci() {
+        memo[0] = 0;
+        memo[1] = 1;
+        for(int i=2; i<memo.length; i++) {
+            memo[i] = -1;
+        }
     }
 
     private final long[] memo = new long[93];
